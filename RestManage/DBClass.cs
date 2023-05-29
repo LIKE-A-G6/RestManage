db34d4e60b7e31975668dc693aed9f93ef2830b9
Постика Андрей Романович <10200291@college.local>
2023-05-29 12:33:21 +0300

    Добавлена авторизация пользоваателей

---
+++ RestManage/DBClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestManage
+{
+    internal class DBClass
+    {
+        SqlConnection sqlConnection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocaldb;Initial Catalog=Restaurant;Integrated Security=True");
+
+        public void openConnection()
+        {
+            if (sqlConnection.State == System.Data.ConnectionState.Closed)
+            {
+                sqlConnection.Open();
+            }
+        }
+
+        public void closeConnection()
+        {
+            if (sqlConnection.State == System.Data.ConnectionState.Open)
+            {
+                sqlConnection.Close();
+            }
+        }
+
+        public SqlConnection getConnection()
+        {
+            return sqlConnection;
+        }
+    }
+}
--- RestManage/MainWindow.xaml.cs
+++ RestManage/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,15 +17,48 @@ using System.Windows.Shapes;
 
 namespace RestManage
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+
+        DBClass dataBase = new DBClass();
         public MainWindow()
         {
             InitializeComponent();
-            int a;
+        }
+
+        private void Registration_window_open(object sender, RoutedEventArgs e)//закрытие окна по кнопке
+        {
+            RegistrationWindow RegistrationWindow = new RegistrationWindow();
+            RegistrationWindow.Show();
+            this.Close();
+        }
+
+        private void LoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string loginUser = LoginTxt.Text;
+            string passwordUser = PasswordTxt.Password;
+
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            DataTable table = new DataTable();
+
+            string querystring = $"select Id, Login, Password from Employee where Login = '{loginUser}' and Password = '{passwordUser}';";
+            SqlCommand command = new SqlCommand(querystring, dataBase.getConnection());
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            if (table.Rows.Count == 1)
+            {
+                MessageBox.Show("Welcome!");
+                LoginTxt.Text = "";
+                PasswordTxt.Password = "";
+            }
+            else
+            {
+                this.Hide();
+                MessageBox.Show("None!");
+                RegistrationWindow regist = new RegistrationWindow();
+                regist.Show();
+            }
         }
     }
 }
